644cc509cfc904b036c432d8925b1a4fdf872b98
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-27 20:27:04 +0200

    next.config: images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    path: `/brushfam-website/_next/public`,
+  },
   assetPrefix: '/brushfam-website',
   basePath: '/brushfam-website'
 }
