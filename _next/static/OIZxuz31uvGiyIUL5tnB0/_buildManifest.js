219626b772d3f54c5459dbe7775d02841648515c
o-tsaruk <90980937+o-tsaruk@users.noreply.github.com>
2023-01-27 18:58:32 +0000

    Deploying to gh-pages from @ 727-Ventures/brushfam-website@054f36a85f5392bbee2c20931b5ac3c6091ac752 🚀

---
+++ _next/static/OIZxuz31uvGiyIUL5tnB0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/241fd82cbf56c170.css","static/chunks/pages/index-68739b0ce0dbdb1b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
