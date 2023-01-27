5b5c11e0594a1531542f44fb73cf4d3c79c97fd0
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-27 17:42:31 +0200

    fix basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/brushfam-website/',
+  basePath: '/brushfam-website',
   assetPrefix: '/brushfam-website/',
 }
 
