ae63c3371de3f7bab15a30db417a9f20f9f89735
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-27 19:42:46 +0200

    basePath: '/brushfam-website/'

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/brushfam-website/public',
-  basePath: '/brushfam-website'
+  assetPrefix: '/brushfam-website',
+  basePath: '/brushfam-website/'
 }
 
 module.exports = nextConfig
