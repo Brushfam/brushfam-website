180506edaf894a326e7b02ed0499a7ecd9e37817
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-27 23:49:44 +0200

    "@next/next/no-img-element": "off"

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    path: "",
-  },
   basePath: '/brushfam-website',
   assetPrefix: '/brushfam-website'
 }
