eae7ceb56dd2c68da9a03e35079e0daa29b1aa64
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-28 01:55:30 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/brushfam-website',
-  assetPrefix: '/brushfam-website'
 }
 
 module.exports = nextConfig
