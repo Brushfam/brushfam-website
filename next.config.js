07758273ce27a3b7ba63a48a7d586c499049e604
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-27 20:39:37 +0200

    path for images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   domains: 'https://727-ventures.github.io/brushfam-website/',
-  //   path: `/brushfam-website/_next/public`,
-  // },
+  images: {
+    // domains: 'https://727-ventures.github.io/brushfam-website/',
+    path: `/brushfam-website/_next/public`,
+  },
   assetPrefix: '/brushfam-website',
   basePath: '/brushfam-website'
 }
