dd744a979e316f10742e881cfa4fd454953f8f0f
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-02-16 20:08:22 +0200

    updated Contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,6 @@
 import styles from "@/styles/Contact.module.css";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Contact() {
   const [email, setEmail] = useState("");
@@ -38,58 +39,48 @@ export default function Contact() {
     });
   };
 
-  return (
-    <section className={styles.section} id={"get-in-touch"}>
-      <p className={styles.titleText}>Get in touch</p>
-      <div className={styles.contactDiv}>
-        <div className={styles.leftDiv}>
-          <p className={styles.titleTextMobile}>Get in touch</p>
-          <h1 className={styles.headerText}>
-            Work with tech experts to win the battle
-          </h1>
-          <p className={styles.contactDescription}>
-            Contact us to get ink! smart contracts audit, advisory, or in case
-            of onboarding your business to Polkadot(web3).
-          </p>
-          <p className={styles.socialTitle}>Also you can contact us via:</p>
-          <div className={styles.contactSocialDiv}>
-            <div className={styles.socialNetwork}>
-              <a href={"https://twitter.com/Brushfam_io"}>
-                <img src={"icons/twitter-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Twitter</p>
-              </a>
-            </div>
-            <div className={styles.socialNetwork}>
-              <a href={"https://medium.com/727-ventures"}>
-                <img src={"icons/medium-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Medium</p>
-              </a>
-            </div>
-            <div className={styles.socialNetwork}>
-              <a
-                href={
-                  "https://matrix.to/#/!utTuYglskDvqRRMQta:matrix.org?via=matrix.org&via=t2bot.io&via=web3.foundation"
-                }
-              >
-                <img src={"icons/element-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Element</p>
-              </a>
-            </div>
-            <div className={styles.lastSocialNetwork}>
-              <a href={"https://discord.gg/EARg6RCThP"}>
-                <img src={"icons/discord-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Discord</p>
-              </a>
-            </div>
+  const SocialButtons = () => {
+    return(
+        <div className={styles.socialWrapper}>
+          <div className={styles.socialRow}>
+            <Link href={"https://twitter.com/Brushfam_io"}>
+              <div className={styles.socialButton}>
+                <img src={"/icons/twitter-social-icon.svg"}/>
+                <p>Twitter</p>
+              </div>
+            </Link>
+            <Link href={"https://medium.com/727-ventures"}>
+              <div className={styles.socialButton}>
+                <img src={"/icons/medium-social-icon.svg"}/>
+                <p>Medium</p>
+              </div>
+            </Link>
+          </div>
+          <div className={styles.socialRow}>
+            <Link href={"https://matrix.to/#/!utTuYglskDvqRRMQta:matrix.org?via=matrix.org&via=t2bot.io&via=web3.foundation"}>
+              <div className={styles.socialButton}>
+                <img src={"/icons/element-social-icon.svg"}/>
+                <p>Element</p>
+              </div>
+            </Link>
+            <Link href={"https://discord.gg/EARg6RCThP"}>
+              <div className={styles.socialButton}>
+                <img src={"/icons/discord-social-icon.svg"}/>
+                <p>Discord</p>
+              </div>
+            </Link>
           </div>
         </div>
+    )
+  }
 
-        <div className={styles.formDiv}>
-          <form method="post" id={"form"} >
-            <label htmlFor={"email"} className={styles.label}>
-              Email
-            </label>
-            <input
+  const ContactForm = () => {
+    return(
+        <form method="post" id={"form"} className={styles.formDiv}>
+          <label htmlFor={"email"} className={styles.label}>
+            Email
+          </label>
+          <input
               type={"email"}
               name={"email"}
               placeholder={"johnsmith@gmail.com"}
@@ -98,11 +89,11 @@ export default function Contact() {
               }}
               className={styles.input}
               required
-            />
-            <label htmlFor={"message"} className={styles.label}>
-              Your Message
-            </label>
-            <input
+          />
+          <label htmlFor={"message"} className={styles.label}>
+            Your Message
+          </label>
+          <input
               type={"text"}
               name={"message"}
               placeholder={"What do you wanna say to us"}
@@ -111,58 +102,45 @@ export default function Contact() {
               }}
               className={styles.input}
               required
-            />
-            {submitted ? (
+          />
+          {submitted ? (
               <p className={styles.submittedText}>Submitted!</p>
-            ) : (
+          ) : (
               <button
-                type="submit"
-                onClick={(e) => {
-                  handleSubmit(e);
-                }}
-                className={styles.button}
+                  type="submit"
+                  onClick={(e) => {
+                    handleSubmit(e);
+                  }}
+                  className={styles.button}
               >
                 Apply
               </button>
-            )}
-          </form>
-        </div>
+          )}
+        </form>
+    )
+  }
 
-        <div className={styles.socialMobile}>
-          <p className={styles.socialTitleMobile}>
-            Also you can contact us via:
+  return (
+    <section className={styles.section} id={"get-in-touch"}>
+      <div className={styles.contactDiv}>
+        <div className={styles.leftDiv}>
+          <p className={styles.headerText}>Get in touch</p>
+          <p className={styles.descriptionText}>
+            Work with tech experts to win the battle
           </p>
-          <div className={styles.contactSocialDivMobile}>
-            <div className={styles.socialNetwork}>
-              <a href={"https://twitter.com/Brushfam_io"}>
-                <img src={"icons/twitter-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Twitter</p>
-              </a>
-            </div>
-            <div className={styles.socialNetwork}>
-              <a href={"https://medium.com/727-ventures"}>
-                <img src={"icons/medium-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Medium</p>
-              </a>
-            </div>
-            <div className={styles.socialNetwork}>
-              <a
-                href={
-                  "https://matrix.to/#/!utTuYglskDvqRRMQta:matrix.org?via=matrix.org&via=t2bot.io&via=web3.foundation"
-                }
-              >
-                <img src={"icons/element-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Element</p>
-              </a>
-            </div>
-            <div className={styles.lastSocialNetwork}>
-              <a href={"https://discord.gg/EARg6RCThP"}>
-                <img src={"icons/discord-blue.svg"} />
-                <p className={styles.socialNetworkTitle}>Discord</p>
-              </a>
-            </div>
-          </div>
+          <SocialButtons></SocialButtons>
         </div>
+          <ContactForm></ContactForm>
+        </div>
+      <div className={styles.contactDivMobileWrapper}>
+        <p className={styles.headerText}>Get in touch</p>
+        <p className={styles.descriptionText}>
+          Work with tech experts to win the battle
+        </p>
+        <div className={styles.contactDivMobile}>
+          <ContactForm></ContactForm>
+        </div>
+        <SocialButtons></SocialButtons>
       </div>
     </section>
   );
