31be10f703b6e1789625aad562551b3f05ce3ec8
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-02-27 16:08:27 +0200

    about us title

--- pages/about-us.js
+++ pages/about-us.js
@@ -5,10 +5,14 @@ import Presentation from "@/sections/about-us/Presentation";
 import Team from "@/sections/about-us/Team";
 import {MobileNavbar} from "@/components/MobileNavbar";
 import styles from "@/styles/bg-styles/bg.module.css"
+import Head from "next/head";
 
 export default function AboutUs() {
     return(
         <div className={styles.aboutUsProperty}>
+            <Head>
+                <title>About us</title>
+            </Head>
             <Navbar></Navbar>
             <MobileNavbar></MobileNavbar>
             <Presentation></Presentation>
