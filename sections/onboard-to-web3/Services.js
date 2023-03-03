5c7669e6a3b2f43d5c990577f31958462f4ea42a
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-03-03 07:08:27 +0200

    web3-onboard-process update

--- sections/onboard-to-web3/Services.js
+++ sections/onboard-to-web3/Services.js
@@ -4,9 +4,8 @@ export default function Services() {
     return(
         <section className={styles.section}>
             <p className={styles.headerText}>Brushfam is ready to:</p>
-            <img src={"/onboard-process.svg"} className={styles.onboardProcessImg} />
-            <p  className={styles.onboardProcessText}>Or we can stay as your support hand on the advisory subscription.</p>
-            <img src={"/onboard-process-mobile.svg"} className={styles.onboardProcessMobileImg} />
+            <img src={"/web3-onboard-process.svg"} className={styles.onboardProcessImg} />
+            <img src={"/web3-onboard-process-mobile.svg"} className={styles.onboardProcessMobileImg} />
         </section>
     )
 }
