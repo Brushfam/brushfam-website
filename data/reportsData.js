6ec917569dfc11dc0c2515b472cbefe7f8cb4d11
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-07-07 15:27:38 +0300

    Panorama Swap audit

--- data/reportsData.js
+++ data/reportsData.js
@@ -1,8 +1,15 @@
 export const reportsData = [
     {
         logo: "/reports/artzero-report.svg",
+        link: "https://artzero.io/",
         header: "ArtZero",
         description: "Number 1 Ink!-based NFT Marketplace on Aleph Zero and Astar Blockchains.",
         file: "/reports/Brushfam-audit-for-ArtZero.pdf",
+    },
+    {
+        logo: "/reports/panorama-swap-report.svg",
+        link: "https://panoramaswap.com/",
+        header: "Panorama Swap",
+        description: "Decentralized exchange built on Aleph Zero",
     }
 ]
