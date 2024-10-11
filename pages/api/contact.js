783b7eb241f582372b29a943d8793bf57dd14826
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-10-11 23:55:35 +0300

    fix warning for anonymous function

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,4 @@
-export default async function (req, res) {
+export default async function contactHandler(req, res) {
   require("dotenv").config();
   let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
